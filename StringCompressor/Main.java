8dce3e9f14979a34931d4e7122501ce2930fcad6
Janbert Jadraque <janbert.jadraque@accenture.com>
2025-07-27 19:22:18 +0800

    update

--- StringCompressor/Main.java
+++ StringCompressor/Main.java
@@ -1,38 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
 
   final static public void main(final String args[]) {
     var foo = new FooImproved();
     char[] chars = {'a','a','b','b','b','b','b','b','b','b','b','b','b','b','c','c','c'};
-    helpPrint(chars);
+    System.out.println("chars: " + Arrays.toString(chars));
 
     int length = foo.compress(chars);
-    helpPrint(chars);
+    System.out.println("charsCompressed: " + Arrays.toString(chars));
 
     System.out.println("length: " + length);
 
 
     char[] chars1 = {'a','b','b','b','b','b','b','b','b','b','b','b','b','c','c','c'};
-    helpPrint(chars1);
+    System.out.println("chars1: " + Arrays.toString(chars1));
     
     int length1 = foo.compress(chars1);
-    helpPrint(chars1);
+    System.out.println("chars1Compressed: " + Arrays.toString(chars1));
 
     System.out.println("length: " + length1);
 
     char[] chars2 = {'a','e', 'b','b','b','b','b','b','b','b','b','b','b','b','d', 'c', 'c', 'e'};
-    helpPrint(chars2);
+    System.out.println("chars2: " + Arrays.toString(chars2));
     
     int length2 = foo.compress(chars2);
-    helpPrint(chars2);
+    System.out.println("chars2Compressed: " + Arrays.toString(chars2));
 
     System.out.println("length: " + length2);
-  }
-  
-  public static void helpPrint(char[] w){
-    System.out.print("[");
-    for(char c : w) {
-      System.out.print("\'" + c++ + "\', ");
-    }
-    System.out.println("]");
   }
 }
