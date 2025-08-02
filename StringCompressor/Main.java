8ba9653012208069c16c945c921d11da411134e3
janbertjadraque <janbert.jadraque@gmail.com>
2025-08-02 22:49:07 +0800

    updated readme for some clarifications

--- StringCompressor/Main.java
+++ StringCompressor/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
   final static public void main(final String args[]) {
     var foo = new FooImproved();
-    char[] chars = {'a','a','b','b','b','b','b','b','b','b','b','b','b','b','c','c','c'};
+    char[] chars = { 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c' };
     System.out.println("chars: " + Arrays.toString(chars));
 
     int length = foo.compress(chars);
@@ -12,18 +12,17 @@ public class Main {
 
     System.out.println("length: " + length);
 
-
-    char[] chars1 = {'a','b','b','b','b','b','b','b','b','b','b','b','b','c','c','c'};
+    char[] chars1 = { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c' };
     System.out.println("chars1: " + Arrays.toString(chars1));
-    
+
     int length1 = foo.compress(chars1);
     System.out.println("chars1Compressed: " + Arrays.toString(chars1));
 
     System.out.println("length: " + length1);
 
-    char[] chars2 = {'a','e', 'b','b','b','b','b','b','b','b','b','b','b','b','d', 'c', 'c', 'e'};
+    char[] chars2 = { 'a', 'e', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'd', 'c', 'c', 'e' };
     System.out.println("chars2: " + Arrays.toString(chars2));
-    
+
     int length2 = foo.compress(chars2);
     System.out.println("chars2Compressed: " + Arrays.toString(chars2));
 
